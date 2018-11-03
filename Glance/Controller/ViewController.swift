90709a8e2d14d0d57b0073dfe3a124376a3b1192
noah <noahwoodward2244@gmail.com>
2018-11-03 13:24:39 -0700

    Added project with orginizing groups

---
+++ Glance/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Glance
+//
+//  Created by Noah Woodward on 11/3/18.
+//  Copyright © 2018 Noah Woodward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
