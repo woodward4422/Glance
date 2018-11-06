cd2e3f413c82c95ff46ca43ad6677e1b6111e44e
noah <noahwoodward2244@gmail.com>
2018-11-05 16:02:28 -0800

    Initial Screen Added

--- Glance/Controller/ViewController.swift
+++ Glance/Controller/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+    }
+    
 
 }
 
